317069563c5d20e6fa3bc8eee54c253107ee750a
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-01-16 15:34:11 -0500

    added initial version of weighted assignment

--- zg/team_assignment.py
+++ zg/team_assignment.py
@@ -1,8 +1,12 @@
+# Kobee Raveendran
+# 
+
 # given a list of names and a number of bins (teams in this case), this will
 # equally (or nearly equally) distribute them across the bins
 import random
 import argparse
 import operator
+import numpy as np
 
 def random_assignment(args):
     # or, if list isn't available, enter line by line
@@ -34,7 +38,7 @@ def random_assignment(args):
     for key, val in groups.items():
         print('\nTeam {}:\n'.format(key + 1))
 
-        for player in groups[key]:
+        for player in val:
             print('   ', player)
 
         print('\n')
@@ -44,6 +48,7 @@ def weighted_assignment(args):
     print('Enter player names and KDR/XLRstats skill levels (separate names and KDR/stats with a space):')
     print('(press Enter again when done entering info)')
 
+    '''
     curr_player_info = input().split(' ')
     
     player_list = [(curr_player_info[0], float(curr_player_info[1]))]
@@ -56,10 +61,59 @@ def weighted_assignment(args):
 
         player_list.append((curr_player_info[0], float(curr_player_info[1])))
 
+    # sorts by second param (either KDR or XLRstats rating)
+    '''
+    player_list = [('Phoenix', 2.86), 
+                   ('Dreamtime', 2.15), 
+                   ('Saood', 2.07), 
+                   ('Reece', 1.51), 
+                   ('meh???', 0.67), 
+                   ('REY305', 6.73), 
+                   ('grav', 3.02)]
     player_list.sort(key = operator.itemgetter(1))
 
-    # sorts by 2nd param (KDR or XLR rating)
-    print(player_list)
+    #print(player_list)
+
+    n = args.num_buckets
+
+    print('\nN: ', n)
+
+    groups = {x: [] for x in range(n)}
+
+    num_players = len(player_list)
+    print(num_players % 2)
+
+    skill_toggle = 1
+
+    while len(player_list) != 0:
+        for i in range(n):
+
+            if len(player_list) == 0:
+                break
+
+            if skill_toggle:
+                groups[i % n].append(player_list.pop(-1))
+
+            else:
+                groups[i % n].append(player_list.pop(0))
+
+        skill_toggle = not skill_toggle
+
+    print('num_players: ', num_players)
+    print('n: ', n)
+
+    if num_players % n != 0:
+        groups[n - 1].append(groups[0].pop(-1))
+
+    for key, val in groups.items():
+        print('\nTeam {}:\n'.format(key + 1))
+        print('   Player Name\t\tKDA/XLR rating')
+
+        for player in val:
+            print('   {}\t\t{}'.format(player[0], player[1]))
+
+        #print('Average skill (KDR/XLR): ', np.mean())
+        print('\n')
 
 if __name__ == '__main__':
 
