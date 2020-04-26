2d5fbc4c8bcdf5e4a1e31dffb49a2d7aa4ef65fc
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-04-26 04:48:48 -0400

    added list version of confidence, added direct t-value calculation

--- sta2023_automate/unit3/confidence.py
+++ sta2023_automate/unit3/confidence.py
@@ -1,4 +1,9 @@
+import sys
+sys.path.append('..')
 import math
+import argparse
+#from sta2023_automate.unit1.var_stddev import std_dev
+from unit1.var_stddev import std_dev
 
 z_table = {
     "80": 1.282,
@@ -13,17 +18,47 @@ z_table = {
 
 if __name__ == '__main__':
 
-    #mean = float(input("Mean: "))
-    std = float(input("Std dev: "))
-    conf = input("Confidence (as %): ")
+    parser = argparse.ArgumentParser()
 
-    n = float(input("n: "))
-    x_bar = float(input("Sample mean (x bar): "))
+    parser.add_argument("--as_list", dest = 'as_list', action = 'store_true')
+    parser.set_defaults(as_list = False)
 
-    conf_interval = str(x_bar) + " +/- " + str(z_table[conf]) + "({}/sqrt({})".format(std, n)
+    args = parser.parse_args()
 
-    print(conf_interval)
+    if args.as_list:
+        from scipy import stats
 
-    bounds = (x_bar - z_table[conf] * (std / math.sqrt(n)), x_bar + z_table[conf] * (std / math.sqrt(n)))
+        vals = input("Enter space-separated values: ").split(' ')
+        data = [float(x) for x in vals]
+        n = len(data)
 
-    print("Bounds:\n", bounds)
+        x_bar = sum(data) / n
+
+        _, std = std_dev(data)
+
+        conf = float(input("Confidence (as %): "))
+        conf /= 100.
+
+        crit_val = stats.t.ppf(1 - ((1 - conf)/2.), n - 1)
+
+        conf_interval = str(x_bar) + " +/- " + str(crit_val) + "({}/sqrt({})".format(std, n)
+
+        bounds = (x_bar - crit_val * (std / math.sqrt(n)), x_bar + crit_val * (std / math.sqrt(n)))
+
+        print("Bounds:\n", bounds)
+
+    else:
+        #mean = float(input("Mean: "))
+        std = float(input("Std dev: "))
+        conf = input("Confidence (as %): ")
+
+        n = float(input("n: "))
+        x_bar = float(input("Sample mean (x bar): "))
+
+        conf_interval = str(x_bar) + " +/- " + str(z_table[conf]) + "({}/sqrt({})".format(std, n)
+
+        print(conf_interval)
+
+        bounds = (x_bar - z_table[conf] * (std / math.sqrt(n)), x_bar + z_table[conf] * (std / math.sqrt(n)))
+
+        print("Bounds:\n", bounds)
