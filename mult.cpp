0ab93b5575a4281569102c2af03b3cb91f0779b9
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-05-06 11:19:07 -0400

    added mult table

---
+++ mult.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+}
+
+vector<vector<int> > generate_table(int upper_bound)
+{
+    vector<vector<int> > products (upper_bound, vector<int> (upper_bound, 0));
+
+    
+}
