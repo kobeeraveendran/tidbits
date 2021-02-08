c780e9edaae7b8b58f365c945348f83290f50d3a
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-02-08 10:30:39 -0500

    try some different params

--- anti_afk.py
+++ anti_afk.py
@@ -6,9 +6,9 @@ i = 1
 while True:
 
     curr_mouse_x, curr_mouse_y = pyautogui.position()
-    x_offset = 5 if i % 2 else -5
+    x_offset = 50 if i % 2 else -50
 
     pyautogui.moveTo(curr_mouse_x + x_offset, curr_mouse_y)
 
     i += 1
-    time.sleep(300)
+    time.sleep(180)
