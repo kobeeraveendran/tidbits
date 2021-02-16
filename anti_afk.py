81e60761cd66036c96e65512e0259e415de253ac
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-02-16 09:54:07 -0500

    add gradual movement, click

--- anti_afk.py
+++ anti_afk.py
@@ -8,7 +8,8 @@ while True:
     curr_mouse_x, curr_mouse_y = pyautogui.position()
     x_offset = 50 if i % 2 else -50
 
-    pyautogui.moveTo(curr_mouse_x + x_offset, curr_mouse_y)
+    pyautogui.moveTo(curr_mouse_x + x_offset, curr_mouse_y, 2)
+    pyautogui.click(button = 'left')
 
     i += 1
-    time.sleep(180)
+    time.sleep(10)
